c097bc4343aabd14e3a4b51300e1f129e001e29a
Quentin T'JAMPENS <pyramond69@gmail.com>
2026-02-22 23:17:15 +0100

    Install firebase

---
+++ firebase-config.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCSgXVua1naewd7chRHxx8pcBOshJOxTG0",
+    authDomain: "oneblind-800bb.firebaseapp.com",
+    projectId: "oneblind-800bb",
+    storageBucket: "oneblind-800bb.firebasestorage.app",
+    messagingSenderId: "483956677247",
+    appId: "1:483956677247:web:3a3f67a011435b8d973d4a",
+    measurementId: "G-FNFSHHZRQQ"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
