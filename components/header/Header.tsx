5593a70b0c7cdf56bde57b925018f2b3b7e5f693
Quentin T'JAMPENS <pyramond69@gmail.com>
2026-03-03 20:39:58 +0100

    feat: use Title component

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,16 +1,11 @@
-import { Heading } from "@radix-ui/themes";
 import { GearIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
+import Title from "@/components/Title/Title";
 
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 border-b w-full p-2 border-zinc-200 bg-white/80 backdrop-blur-md dark:border-zinc-800 dark:bg-zinc-900/95 flex flex-row items-center justify-between">
-      <Heading
-        size="8"
-        className="font-bold text-indigo-600 dark:text-indigo-400 p-2"
-      >
-        Oneblind
-      </Heading>
+      <Title level={"h1"}>Oneblind</Title>
 
       <Link
         href="/settings"
