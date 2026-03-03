2ba2058c598bdd01365a532a8a02a870c50f0b75
Quentin T'JAMPENS <pyramond69@gmail.com>
2026-03-03 21:53:47 +0100

    feat: Add user to firebase

--- app/[locale]/players/components/CreatePlayer.tsx
+++ app/[locale]/players/components/CreatePlayer.tsx
@@ -3,12 +3,26 @@
 import { Button, Dialog, Flex, TextField } from "@radix-ui/themes";
 import { useI18n } from "@/locales/client";
 import { useState } from "react";
+import { addDoc, collection, Timestamp } from "firebase/firestore";
+import { db } from "@/firebase-config";
 
 export default function CreatePlayer() {
   const t = useI18n();
   const [userName, setUserName] = useState<string>("");
 
-  function create() {}
+  async function create() {
+    if (!userName) return;
+
+    try {
+      await addDoc(collection(db, "users"), {
+        name: userName,
+        creationDate: Timestamp.fromDate(new Date()),
+        points: 0,
+      });
+    } catch (error) {
+      console.error("Error :", error);
+    }
+  }
 
   return (
     <>
