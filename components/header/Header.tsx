2aabdb8bfaf639c48b60adc569c9dc600a2f3b67
Quentin T'JAMPENS <pyramond69@gmail.com>
2026-03-08 15:18:37 +0100

    style: use RadixUI IconButton

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,17 +1,17 @@
 import { GearIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 import Title from "@/components/Title/Title";
+import { IconButton } from "@radix-ui/themes";
 
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 border-b w-full p-2 border-zinc-200 bg-white/80 backdrop-blur-md dark:border-zinc-800 dark:bg-zinc-900/95 flex flex-row items-center justify-between">
       <Title level={"h1"}>Oneblind</Title>
 
-      <Link
-        href="/settings"
-        className="p-2 text-indigo-600 dark:text-indigo-400 hover:opacity-80 transition-opacity"
-      >
-        <GearIcon width={35} height={35} />
+      <Link href="/settings" className="p-2">
+        <IconButton variant={"ghost"} radius={"full"}>
+          <GearIcon width={35} height={35} />
+        </IconButton>
       </Link>
     </header>
   );
