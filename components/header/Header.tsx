a7738ac4e7599de4a12ab6907de98d9b12a2ee49
Quentin T'JAMPENS <pyramond69@gmail.com>
2026-03-10 14:09:16 +0100

    feat: Change logo theme

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,12 +1,12 @@
 import { GearIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
-import Title from "@/components/Title/Title";
 import { IconButton } from "@radix-ui/themes";
+import HeaderTitle from "@/components/header/components/HeaderTitle";
 
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 border-b w-full p-2 border-zinc-200 bg-white/80 backdrop-blur-md dark:border-zinc-800 dark:bg-zinc-900/95 flex flex-row items-center justify-between">
-      <Title level={"h1"}>Oneblind</Title>
+      <HeaderTitle />
 
       <Link href="/settings" className="p-2">
         <IconButton variant={"ghost"} radius={"full"}>
