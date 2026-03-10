94894e9bdd54dac74a692ba6b44646824477f0b0
Quentin T'JAMPENS <pyramond69@gmail.com>
2026-03-10 09:04:43 +0100

    feat: Add title

--- app/[locale]/settings/components/RadiusSelection.tsx
+++ app/[locale]/settings/components/RadiusSelection.tsx
@@ -3,6 +3,7 @@
 import { Select } from "@radix-ui/themes";
 import { useSettings } from "@/contexts/SettingsContext";
 import { RadiusType, radiusTypes } from "@/interfaces/radius.interface";
+import Title from "@/components/Title/Title";
 
 export default function RadiusSelection() {
   const { setRadius, radius } = useSettings();
@@ -14,20 +15,26 @@ export default function RadiusSelection() {
   }
 
   return (
-    <Select.Root
-      defaultValue={radius}
-      onValueChange={(value) => changeRadius(value)}
-    >
-      <Select.Trigger />
-      <Select.Content>
-        <Select.Group>
-          {radiusTypes.map((item, index) => (
-            <Select.Item key={index} value={item}>
-              {item}
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Title level={"h3"}>Changer les bordures</Title>
+
+      <div className={"m-3"}>
+        <Select.Root
+          defaultValue={radius}
+          onValueChange={(value) => changeRadius(value)}
+        >
+          <Select.Trigger />
+          <Select.Content>
+            <Select.Group>
+              {radiusTypes.map((item, index) => (
+                <Select.Item key={index} value={item}>
+                  {item}
+                </Select.Item>
+              ))}
+            </Select.Group>
+          </Select.Content>
+        </Select.Root>
+      </div>
+    </>
   );
 }
