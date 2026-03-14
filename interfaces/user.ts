547e61c8831856a762444f67b4af3156de4dded5
Quentin T'JAMPENS <pyramond69@gmail.com>
2026-03-14 14:28:50 +0100

    refac: Create DateInterface and use it in user interface

--- interfaces/user.ts
+++ interfaces/user.ts
@@ -1,9 +1,8 @@
+import { DateInterface } from "@/interfaces/date.interface";
+
 export default interface User {
   id: string;
   name: string;
   points: number;
-  creationDate: {
-    seconds: number;
-    nanoseconds: number;
-  };
+  creationDate: DateInterface;
 }
