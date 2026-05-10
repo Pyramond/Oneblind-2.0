58f2a84818ca2834577da2c7797685d9e499c262
Quentin T'JAMPENS <pyramond69@gmail.com>
2026-05-10 16:22:13 +0200

    style: set RebuyBtn color to gray

--- app/[locale]/tournaments/[id]/components/RebuyBtn.tsx
+++ app/[locale]/tournaments/[id]/components/RebuyBtn.tsx
@@ -1,4 +1,3 @@
-import { Tournament } from "@/interfaces/tournament.interface";
 import { ReactNode } from "react";
 import { Button } from "@radix-ui/themes";
 import { useI18n } from "@/locales/client";
@@ -8,5 +7,9 @@ export default function RebuyBtn(): ReactNode {
   const t = useI18n();
   const { addRebuy } = useTournamentRunner();
 
-  return <Button onClick={addRebuy}>{t("tournaments.runner.rebuy")}</Button>;
+  return (
+    <Button onClick={addRebuy} color={"gray"}>
+      {t("tournaments.runner.rebuy")}
+    </Button>
+  );
 }
