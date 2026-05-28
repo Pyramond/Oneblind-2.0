9b42a05f222d3d51d3834f3ac5a94dc320edb964
Quentin T'JAMPENS <pyramond69@gmail.com>
2026-05-28 18:49:51 +0200

    feat: Add className arg

--- components/Title/Title.tsx
+++ components/Title/Title.tsx
@@ -7,15 +7,16 @@ import { useSettings } from "@/contexts/SettingsContext";
 interface TitleProps {
   level: "h1" | "h2" | "h3" | "h4";
   children: React.ReactNode;
+  className?: string;
 }
 
-export default function Title({ level, children }: TitleProps) {
+export default function Title({ level, children, className }: TitleProps) {
   const { color } = useSettings();
 
   switch (level) {
     case "h1":
       return (
-        <Heading as={"h1"} size="8" color={color}>
+        <Heading as={"h1"} size="8" color={color} className={className}>
           {children}
         </Heading>
       );
@@ -24,7 +25,7 @@ export default function Title({ level, children }: TitleProps) {
         <Heading
           as={"h2"}
           size={"7"}
-          className="font-bold text-zinc-800 dark:text-zinc-200 p-2"
+          className={`font-bold text-zinc-800 dark:text-zinc-200 p-2 ${className ?? ""}`}
         >
           {children}
         </Heading>
@@ -35,7 +36,7 @@ export default function Title({ level, children }: TitleProps) {
         <Heading
           as="h3"
           size="6"
-          className="font-semibold text-zinc-700 dark:text-zinc-300 p-2"
+          className={`font-semibold text-zinc-700 dark:text-zinc-300 p-2 ${className ?? ""}`}
         >
           {children}
         </Heading>
@@ -46,7 +47,7 @@ export default function Title({ level, children }: TitleProps) {
         <Heading
           as="h4"
           size="5"
-          className="font-medium text-zinc-700 dark:text-zinc-300 p-2"
+          className={`font-medium text-zinc-700 dark:text-zinc-300 p-2 ${className ?? ""}`}
         >
           {children}
         </Heading>
