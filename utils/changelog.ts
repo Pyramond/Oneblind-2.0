07cd01060e38a0bd7e77bca70a2cdae46afc1bfb
Quentin TJAMPENS <pyramond69@gmail.com>
2026-06-26 00:01:08 +0200

    chore(changelog): Update changelog

--- utils/changelog.ts
+++ utils/changelog.ts
@@ -19,7 +19,9 @@ export const changelog: ChangelogEntry[] = [
         "Thème light pour le header du runner de tournoi",
         "Site de documentation en ligne (FR / EN)",
         "Améliorations de l'UI/UX",
-          "Export/Import des structure des blinds en JSON"
+        "Export/Import des structure des blinds en JSON",
+        "Correction des bouton retour",
+        "Correction de l'URL de retour lors d'une connextion Spotify"
       ],
       en: [
         "Spotify: display currently playing track in tournament",
@@ -28,7 +30,9 @@ export const changelog: ChangelogEntry[] = [
         "Light theme for tournament runner header",
         "Online documentation site (FR / EN)",
         "UI/UX improvements",
-          "Export/Import blind structures into JSON file"
+        "Export/Import blind structures into JSON file",
+        "Fix back buttons",
+        "Fix back URL in spotify connection"
       ],
     },
   },
